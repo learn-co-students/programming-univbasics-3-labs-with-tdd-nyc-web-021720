ca3b5ea5fc009c31f173b8f3a8d437bf50ebde6a
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-01-29 06:07:37 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
 # Add your variables here
+# Value for the 1st number.
+first_number = 2
+second_number = 4
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number 
